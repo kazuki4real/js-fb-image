565673c631d313d497b0003fef3f7ad93d5d83a6
kazuki4real <60100672+kazuki4real@users.noreply.github.com>
2020-02-12 23:14:33 +0900

    js added

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
-'use strict';
-{
+class FBGrid {
+  constructor(selector, option) {
+    const ele = typeof selector === 'string' ? document.querySelectorAll(selector) : selector;
+    this.ele = ele;
+    this.run();
+  }
 
-  
-  // const images = [
-  //   'img/mount_rushmore.jpg',
-  //   'img/united-states-usa-america-monument-valley-arizona-utah-famous-place-monument-valley-landscape-arizona-utah-scene-navajo-121420678.jpg',
-  //   'img/usa-best-places-new-york.jpg',
-  //   'img/usa-best-places-san-francisco.jpg',
-  //   'img/usa-best-places-washington.jpg',
-  // ];
-
-
-  // const container = document.getElementById('container');
-  
-  // images.forEach(image => {
-  //   const div = document.createElement('div');
-  //   div.classList.add('item');
-  //   const img = document.createElement('img');
-  //   img.src = image;
-  // });
-
-
-
-  // container.appendChild(div);
-  // div.appendChild(img);
-
+    run() {
+    [].forEach.call(this.ele, (ele) => {
+      const count = ele.childElementCount;
+      ele.classList.add(`pattern${count}`);
+      const imgs = ele.querySelectorAll('img');
+      const imgArry = [].map.call(imgs, img => img.src);
+      ele.innerHTML = `${imgArry.map((img) => {
+        return `<div class="item"><img src=${img} /></div>`
+      }).join('')}`;
+    });
+  }
 }
