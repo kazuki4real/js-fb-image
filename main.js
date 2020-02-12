f94f0fff3fe6b3c9e27a0aba950d909cf2792e07
kazuki4real <60100672+kazuki4real@users.noreply.github.com>
2020-02-12 15:53:38 +0900

    css patterns added

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 'use strict';
 {
+
   
+  // const images = [
+  //   'img/mount_rushmore.jpg',
+  //   'img/united-states-usa-america-monument-valley-arizona-utah-famous-place-monument-valley-landscape-arizona-utah-scene-navajo-121420678.jpg',
+  //   'img/usa-best-places-new-york.jpg',
+  //   'img/usa-best-places-san-francisco.jpg',
+  //   'img/usa-best-places-washington.jpg',
+  // ];
+
+
+  // const container = document.getElementById('container');
+  
+  // images.forEach(image => {
+  //   const div = document.createElement('div');
+  //   div.classList.add('item');
+  //   const img = document.createElement('img');
+  //   img.src = image;
+  // });
+
+
+
+  // container.appendChild(div);
+  // div.appendChild(img);
+
 }
