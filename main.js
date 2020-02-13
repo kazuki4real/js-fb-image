69444c490cda8d2b2257917e2f123158668d5a14
kazuki4real <60100672+kazuki4real@users.noreply.github.com>
2020-02-13 16:50:41 +0900

    javascript and css changed

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ class FBGrid {
     run() {
     [].forEach.call(this.ele, (ele) => {
       const count = ele.childElementCount;
-      
+      const imageNumber = document.querySelectorAll('img');
+        //if the first image is the horizontal rectangle...
+      if (imageNumber[0].naturalWidth > imageNumber[0].naturalHeight) {
+        ele.classList.add(`pattern${count}`);
+      } else {  //if the first image is vertical rectangle...
         ele.classList.add(`pattern${count}_a`);
-      
+      }
         const imgs = ele.querySelectorAll('img');
         const imgArry = [].map.call(imgs, img => img.src);
         ele.innerHTML = `${imgArry.map((img) => {
